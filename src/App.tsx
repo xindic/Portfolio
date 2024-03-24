099fed1570e0e6c81b79a9815421d8dff905641c
xindic <ayaanlums@gmail.com>
2024-03-24 14:38:57 +0500

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter basename={import.meta.env.DEV ? "/" : "/portfolio/"}>
       <Routes>
-        <Route path="/portfolio" element={<Work />} />
+        <Route path="/" element={<Work />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
