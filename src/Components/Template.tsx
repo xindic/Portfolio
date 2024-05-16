b8b4205a47bbed80cc8caace550b54dd1119e98b
xindic <ayaanlums@gmail.com>
2024-05-16 14:48:02 +0500

    Updated template

--- src/Components/Template.tsx
+++ src/Components/Template.tsx
@@ -1,26 +1,42 @@
 interface Props {
-  Name?: string;
+  Name: string;
   Image?: string;
   Description?: string;
-  Link?: string;
+  Link: string;
   Video?: string;
 }
 
 const MouseEnter = (event: React.MouseEvent<HTMLElement>) => {
+  const title =
+    event.currentTarget.parentElement?.parentElement?.querySelector("h1");
+
   event.currentTarget.style.opacity = "1";
+  if (title) {
+    title.style.opacity = "1";
+  }
 };
 
 const MouseLeave = (event: React.MouseEvent<HTMLElement>) => {
+  const title =
+    event.currentTarget.parentElement?.parentElement?.querySelector("h1");
+
   event.currentTarget.style.opacity = "0.7";
+  if (title) {
+    title.style.opacity = "0.5";
+  }
 };
 
-// const MouseDown = (event: React.MouseEvent<HTMLElement>) => {
-// };
-
 const Template = ({
+  Name,
   Image = "https://htmlcolors.com/color-image/111111.png",
   Video,
+  Link,
 }: Props) => {
+  const handleClick = (event: React.MouseEvent<HTMLImageElement>) => {
+    event.preventDefault();
+    window.open(Link, "_blank");
+  };
+
   if (Video) {
     return (
       <video
@@ -33,15 +49,20 @@ const Template = ({
     );
   } else {
     return (
-      <div className="mx-8 overflow-hidden rounded-sm">
-        <img
-          src={Image}
-          className="w-[100%] h-[100%] opacity-[0.7]"
-          onMouseEnter={MouseEnter}
-          onMouseLeave={MouseLeave}
-          // onMouseDown={MouseDown}
-        />
-      </div>
+      <>
+        <div className="mx-8 overflow-hidden">
+          <h1 className="opacity-[0.5] my-3 text-[1.2rem]"> {Name} </h1>
+          <div>
+            <img
+              src={Image}
+              className="w-[100%] h-[100%] opacity-[0.7] rounded-xl"
+              onMouseEnter={MouseEnter}
+              onMouseLeave={MouseLeave}
+              onMouseDown={handleClick}
+            />
+          </div>
+        </div>
+      </>
     );
   }
 };
--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -5,13 +5,33 @@ const Projects = () => {
   return (
     <div className="w-[100vw] h-[100vh]">
       <NavigationBar />
-      <div className="w-[80vw] h-[85vh] mx-[10vw] Scrollable">
+      <div className="w-[85vw] h-[85vh] mx-[7.5vw] Scrollable">
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full sm:mb-4 gap-y-10">
-          <Template Image="https://tr.rbxcdn.com/1f20694c727ba2bab87fd663d2c622df/768/432/Image/Png" />
-          <Template Image="https://tr.rbxcdn.com/acadd395d5a1726dcacb207d458c911c/768/432/Image/Png" />
-          <Template Image="https://tr.rbxcdn.com/2aeffb8f1c9d881a55357516ba4f6575/500/280/Image/Jpeg" />
-          <Template Image="https://tr.rbxcdn.com/46a2f6114fde2705d6d8e25777008cd1/768/432/Image/Png" />
-          <Template Image="https://tr.rbxcdn.com/8d31645e27780802eb271cd2458c7fc2/768/432/Image/Png" />
+          <Template
+            Name="Arm Wrestling Simulator"
+            Link="https://www.roblox.com/games/14985614364/Weight-Lifting-Simulator"
+            Image="https://tr.rbxcdn.com/1f20694c727ba2bab87fd663d2c622df/768/432/Image/Png"
+          />
+          <Template
+            Name="Mining Adventure Simulator"
+            Link="https://www.roblox.com/games/15618465191/Mining-Adventure-Simulator"
+            Image="https://tr.rbxcdn.com/acadd395d5a1726dcacb207d458c911c/768/432/Image/Png"
+          />
+          <Template
+            Name="Tree Farming Simulator"
+            Link="https://www.roblox.com/games/14742393991/Tree-Farming-Simulator"
+            Image="https://tr.rbxcdn.com/2aeffb8f1c9d881a55357516ba4f6575/500/280/Image/Jpeg"
+          />
+          <Template
+            Name="The Conductor"
+            Link="https://www.roblox.com/games/16130519846/The-Conductor"
+            Image="https://tr.rbxcdn.com/46a2f6114fde2705d6d8e25777008cd1/768/432/Image/Png"
+          />
+          <Template
+            Name="Tarnished"
+            Link="https://www.roblox.com/games/15077806640/Tarnished"
+            Image="https://tr.rbxcdn.com/8d31645e27780802eb271cd2458c7fc2/768/432/Image/Png"
+          />
         </div>
       </div>
     </div>
